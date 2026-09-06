022fb02fcfcd23b0280b58c35f4f8789f265ca51
Suhanee Thapa <suhaneethapa@gmail.com>
2026-09-06 14:18:55 +0545

    fix: prevent browser cache for authenticated users + vertical layout fixes

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.NoCacheMiddleware",  # <-- ADD THIS LINE
 ]
 
 ROOT_URLCONF = "config.urls"
